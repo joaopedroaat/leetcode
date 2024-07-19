829ddc1670e7930940e5d96974848986ae31625f
joaopedroaat <joaopedroaat@outlook.com>
2024-07-18 23:34:11 -0300

    Better two_sum solution

--- answers/two_sum.go
+++ answers/two_sum.go
@@ -1,12 +1,14 @@
 package answers
 
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if (nums[i] + nums[j]) == target {
-				return []int{i, j}
-			}
+	prevMap := make(map[int]int) // val -> index
+
+	for i, n := range nums {
+		diff := target - n
+		if _, ok := prevMap[diff]; ok {
+			return []int{prevMap[diff], i}
 		}
+		prevMap[n] = i
 	}
 
 	return []int{}
